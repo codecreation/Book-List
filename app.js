d167cf7dc66a749e96b8c0bc9bd0e421455bb452
Amir Tamimi <amir.tamim@trojans.dsu.edu>
2019-09-11 18:40:47 -0700

    Wrote UI. Book and UI constructor classes with ES5

---
+++ app.js
@@ -0,0 +1,14 @@
+//book constr
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//ui constr
+function UI() {
+
+}
+
+//events
+document.getElementById('book-form').addEventListener
